1935f830167feecb7b408a92074b82abc1d55a03
Ethan <eleetyson@gmail.com>
2020-09-14 12:17:01 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,6 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to song_path(@song)
     else
@@ -26,15 +25,22 @@ class SongsController < ApplicationController
   end
 
   def update
-
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
   end
 
   private
 
   def song_params(*args)
-    params.require(:song).permit(*args)
+    params.require(:song).permit(:artist_name, :genre, :release_year, :released, :title)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ApplicationRecord
   # title can't be blank or repeated by the same artist in the same year
+  # artist name can't be blank
   # released must be true or false
   # release year, if song is released...
     # should exist be less than or equal to the current year
-  # artist name can't be blank
   validates :title, presence: true
   validates :title, uniqueness: { scope: [:artist_name, :release_year] }
   validates :artist_name, presence: true
