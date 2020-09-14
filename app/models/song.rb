235f42b4afca3775f4d93b8933ef2e6efed22b34
Ethan <eleetyson@gmail.com>
2020-09-14 11:30:24 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,14 +5,14 @@ class Song < ApplicationRecord
     # should exist be less than or equal to the current year
   # artist name can't be blank
   validates :title, presence: true
-  validates_uniqueness_of :title, scope: [:artist_name, :release_year]
+  validates :title, uniqueness: { scope: [:artist_name, :release_year] }
   validates :artist_name, presence: true
-  validates :released, inclusion: {in: [true, false]}
+  validates :released, inclusion: { in: [true, false] }
+
   with_options if: :song_released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: {less_than_or_equal_to: Time.now.year}
   end
-
 
   def song_released?
     released
